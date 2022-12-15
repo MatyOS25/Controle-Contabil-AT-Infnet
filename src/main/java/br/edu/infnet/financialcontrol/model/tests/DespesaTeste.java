3211038ae963c4bd2655de09a31c033f6061fe46
MatyOS25 <matiasreinke@hotmail.com>
2022-12-15 17:33:12 -0300

    AT #1

---
+++ src/main/java/br/edu/infnet/financialcontrol/model/tests/DespesaTeste.java
@@ -0,0 +1,10 @@
+package br.edu.infnet.financialcontrol.model.tests;
+
+import br.edu.infnet.financialcontrol.model.domain.Provisao;
+
+public class DespesaTeste {
+	public static void main(String[] args) {
+		Provisao c1 = new Provisao();
+		
+	}
+}
